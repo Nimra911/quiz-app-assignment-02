bd3cefae0fdd4fbcfe7151d81d066c8f68e9d4dc
Nimra911 <80028962+Nimra911@users.noreply.github.com>
2021-04-21 20:51:05 +0500

    Add files via upload
    color changes

---
+++ constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Colors.blueGrey;
+const kGreenColor = Colors.green;
+const kRedColor = Colors.red;
+const kGrayColor = Colors.grey;
+const kBlackColor = Colors.black;
+const kPrimaryGradient = LinearGradient(
+  colors: [Colors.greenAccent, Colors.blueGrey],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+const double kDefaultPadding = 20.0;
