3885939fc7b69a84b087b2fb51b4a983a03019ae
Nimra911 <80028962+Nimra911@users.noreply.github.com>
2021-04-21 15:22:23 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:quiz_app/screens/welcome/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
