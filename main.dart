6c601b6f68818da9160cae7cd5117665f7279eb2
Nimra911 <80028962+Nimra911@users.noreply.github.com>
2021-04-24 20:38:56 +0500

    Add files via upload
    main file after some changes

--- main.dart
+++ main.dart
@@ -1,20 +1,30 @@
+
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:quiz_app/screens/welcome/welcome_screen.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'selected_type.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Quiz App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.fallback(),
-      home: WelcomeScreen(),
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "Quiz App",
+  home: mysplashscreen(),
+));
+
+class mysplashscreen extends StatelessWidget{
+  Widget build(BuildContext context){
+    return new SplashScreen(seconds: 0,
+      navigateAfterSeconds: new selecttype(),
+      title: new Text("Nimra Rehman(Fa17-bcs-072)",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0,color: Colors.lime,),
+    ),
+      loadingText: new Text("Page loading...", style: new TextStyle( fontSize: 30.0,color: Colors.white,),),
+
+      backgroundColor: Colors.blueGrey,
+      styleTextUnderTheLoader: new TextStyle(),
+      loaderColor: Colors.greenAccent,
     );
   }
 }
+
+
