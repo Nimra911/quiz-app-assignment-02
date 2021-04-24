061a41fa85e6ca9989ef94274f7b2e2a252ea985
Nimra911 <80028962+Nimra911@users.noreply.github.com>
2021-04-24 21:13:17 +0500

    Add files via upload
    after some changes in welcome screen

--- welcome_screen.dart
+++ welcome_screen.dart
@@ -1,59 +1,188 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:get/get.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+import 'package:quiz_app/true_false_quiz.dart';
+import 'package:quiz_app/multiplechoice.dart';
+import 'package:quiz_app/contactus.dart';
+import 'package:quiz_app/bouncinbutton.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
+
+      drawer: Theme(
+        data: Theme.of(context).copyWith( //This will change the drawer background to blue.
+          canvasColor: Colors.cyan,
+        ),
+        child: Drawer(
+            child: ListView(
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Nimra Rehman"),
+                  accountEmail: Text("nimrarehmanvehari@gmail.com"),
+                  decoration: BoxDecoration(
+                    gradient: new LinearGradient(
+                        colors: [
+                          Colors.cyan,
+                          Colors.cyan.shade300,
+                        ],
+
+                        stops: [0.0, 1.0],
+                      ),
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Theme.of(context).platform == TargetPlatform.iOS
+                        ? const Color(0xFF00897b)
+                        : Colors.white,
+                    child: Text(
+                      "N",
+                      style: TextStyle(fontSize: 40.0),
+                    ),
+                  ),
+                ),
+                Divider(
+                  height: 2,
+                  thickness: 2,
+                ),
+                ListTile(
+                  title: Container(
+                    padding: EdgeInsets.all(kDefaultPadding * 0.80), // 15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+
+                    ),
+                    child: Text('True or False',style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: Colors.white, fontSize: 20.0),
+
+                    ),
+                  ),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>getjson2("True or False")),
+                    );
+
+                  },
+                ),
+                Divider(
+                  height: 2,
+                  thickness: 2,
+                ),
+                ListTile(
+                  title: Container(
+                    padding: EdgeInsets.all(kDefaultPadding * 0.80), // 15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+
+                    ),
+                    child: Text('M C Qs',style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: Colors.white, fontSize: 20.0),
+
+                    ),
+                  ),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => multiplechoice()),
+                    );
+
+                  },
+                ),
+                Divider(
+                  height: 2,
+                  thickness: 2,
+                ),
+                ListTile(
+                  title: Container(
+                    padding: EdgeInsets.all(kDefaultPadding * 0.80), // 15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+
+                    ),
+                    child: Text('Contact Us',style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: Colors.white, fontSize: 20.0),
+
+                    ),
+                  ),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => contactus()),
+                    );
+
+                  },
+                ),
+                Divider(
+                  height: 2,
+                  thickness: 2,
+                ),
+                ListTile(
+                  title: Container(
+                    padding: EdgeInsets.all(kDefaultPadding * 0.80), // 15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+
+                    ),
+                    child: Text('Animation Button',style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: Colors.white, fontSize: 20.0),
+
+                    ),
+                  ),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BouncingButton()),
+                    );
+
+                  },
+                ),
+                Divider(
+                  height: 2,
+                  thickness: 2,
+                ),
+
+
+              ],
+            )
+        ),
+      ),
       body: Stack(
         children: [
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    width: 200.0,
-                    height: 80.0,
+                  Spacer(flex: 2),
+                   Text(
+                    "Let's Play Quiz,",
+                    style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
                   ),
-
-                  Text(
-                    "Let's Play Quiz",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline2.copyWith(
-                        color: Colors.purple.shade900,fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200.0,
-                    height: 10.0,
-                  ),
-                  Text("Enter your information below",
-                  style: TextStyle(
-                    color: Colors.purple),
-                  ),
-                  SizedBox(
-                    width: 200.0,
-                    height: 40.0,
-
-                  ),
+                  Spacer(), // 1/6
                   TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Colors.blue,
-                      labelText: 'Full Name',
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Enter your Full Name",
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        borderRadius: BorderRadius.all(Radius.circular(0)),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 200.0,
-                    height: 40.0,
-                  ),
+                  Spacer(), // 1/6
                   InkWell(
                     onTap: () => Get.to(QuizScreen()),
                     child: Container(
@@ -62,18 +191,18 @@ class WelcomeScreen extends StatelessWidget {
                       padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
                       decoration: BoxDecoration(
                         gradient: kPrimaryGradient,
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
                       ),
                       child: Text(
-                        "Lets Start Quiz",
+                        "Lets Play the Quiz",
                         style: Theme.of(context)
                             .textTheme
                             .button
-                            .copyWith(color: Colors.purple,fontSize: 20),
+                            .copyWith(color: Colors.black),
                       ),
                     ),
                   ),
-                  // it will take 2/6 spaces
+                  Spacer(flex: 2), // it will take 2/6 spaces
                 ],
               ),
             ),
